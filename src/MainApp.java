74440be1b26bf6a68ff05739e0c616e298e6aee3
Pawel Kozubala <p.kozubala@gmail.com>
2021-01-08 07:16:20 +0100

    check if sum of numbers are even or not

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,6 +1,19 @@
 public class MainApp {
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        int a = 32346;
+        int b = 77897;
+        int c = 43443;
+        int d = 26323;
+
+        int sumAB = a + b;
+        int sumDC = c + d;
+
+        if (sumAB % 2 == 0 && sumDC % 2 == 0) {
+            System.out.println("Wszystkie liczby są parzyste");
+        } else {
+            System.out.println("Liczby nie są jest parzyste");
+        }
     }
 }
+
