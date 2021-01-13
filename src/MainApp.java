4d0b716fe19df6ba6eccb25b9b6747e13ddf9006
Pawel Kozubala <p.kozubala@gmail.com>
2021-01-13 07:51:05 +0100

    create table

--- src/MainApp.java
+++ src/MainApp.java
@@ -5,21 +5,7 @@ public class MainApp {
     public static void main(String[] args) {
 
 
-        int[] numbers = {2, 2, 3, 4, 5, 7, 7, 7, 8, 8, 2};
-
-        int even = 0;
-        int odd = 0;
-
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                even += 1;
-            } else {
-                odd += 1;
-            }
-        }
-
-        System.out.println("Ilosc liczb parzystych: "+even);
-        System.out.println("Ilosc liczb nieparzystych: "+odd);
+        int[] numbers = {2, 4, 1, 9, 5, 2, 4, 8, 5, 8};
 
 
     }
