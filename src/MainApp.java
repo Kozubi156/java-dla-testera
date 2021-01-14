b0a1bef9fcb2fa21bcf732e8a450dcb37ba3fd45
Pawel Kozubala <p.kozubala@gmail.com>
2021-01-14 05:45:05 +0100

    create

--- src/MainApp.java
+++ src/MainApp.java
@@ -4,8 +4,21 @@ import model.User;
 public class MainApp {
     public static void main(String[] args) {
 
-
         int[] numbers = {2, 4, 1, 9, 5, 2, 4, 8, 5, 8};
+
+        int min;
+
+        for (int i = 0; i < numbers.length; i++) {
+            min = numbers[i];
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[j] < min) {
+                    min = numbers[j];
+                    numbers[j] = numbers[i];
+                    numbers[i] = min;
+                }
+            }
+
+        }
 
 
     }
