00c3410aa447f8c0360a35fe00438fd1c1914f30
Pawel Kozubala <p.kozubala@gmail.com>
2021-01-14 06:10:24 +0100

    resolution

--- src/MainApp.java
+++ src/MainApp.java
@@ -8,16 +8,25 @@ public class MainApp {
 
         int min;
 
-        for (int i = 0; i < numbers.length; i++) {
+        for(int i = 0;i<numbers.length;i++ ){
             min = numbers[i];
-            for (int j = i + 1; j < numbers.length; j++) {
-                if (numbers[j] < min) {
+            for(int j = i+1;j<numbers.length;j++){
+                if(numbers[j] < min){
                     min = numbers[j];
                     numbers[j] = numbers[i];
                     numbers[i] = min;
+
                 }
+
             }
 
+        }
+
+
+
+
+        for (int number : numbers) {
+            System.out.println(number);
         }
 
 
