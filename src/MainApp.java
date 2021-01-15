4cf48e2cecb796bfceb21c06c2741f20e63b3393
Pawel Kozubala <p.kozubala@gmail.com>
2021-01-15 05:39:47 +0100

    add finale variable

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,17 +1,23 @@
 import model.Bug;
 import model.User;
 import utils.StringUtils;
+import utils.WeekUtils;
+
+import java.util.Map;
 
 public class MainApp {
     public static void main(String[] args) {
 
-        User user1 = new User("Paweł","Robak","robal@gmail.com",80);
-        User user2 = new User("Paweł","Robak","robal@gmail.com",80);
-        User user3 = new User("Paweł","Robak","robal@gmail.com",80);
-        User user4 = new User("Paweł","Robak","robal@gmail.com",80);
-        User user5 = new User("Paweł","Robak","robal@gmail.com",80);
+        User user1 = new User("Paweł", "Robak", "robal@gmail.com", 80);
+        User user2 = new User("Paweł", "Robak", "robal@gmail.com", 80);
+        User user3 = new User("Paweł", "Robak", "robal@gmail.com", 80);
+        User user4 = new User("Paweł", "Robak", "robal@gmail.com", 80);
+        User user5 = new User("Paweł", "Robak", "robal@gmail.com", 80);
 
         System.out.println(StringUtils.getFormattedText("sfdfjdhfdkjhdfjdkdsjkdsfs"));
+
+        System.out.println(WeekUtils.MONDAY);
+
 
     }
 }
