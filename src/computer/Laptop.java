251ddc4197fee2ea33c02b8da02008e74d05ee50
Pawel Kozubala <p.kozubala@gmail.com>
2021-01-20 06:12:25 +0100

    Changed contidion for numbers range 1-100 to grather than 0

--- src/computer/Laptop.java
+++ src/computer/Laptop.java
@@ -21,7 +21,7 @@ public class Laptop extends Computer {
 
     @Override
     public int volumeUp(int volume) {
-        if(volume > 0 && volume <= 100) {
+        if(volume > 0) {
             if (volumeLevel >= 100) {
                 volumeLevel = 100;
                 System.out.println("Max volume level 100 reached");
@@ -29,7 +29,7 @@ public class Laptop extends Computer {
                 volumeLevel += volume;
             }
         } else {
-            System.out.println("Wrong number, you can enter number form range 1-100");
+            System.out.println("Wrong number, only positive numbers are allowed");
         }
         return volumeLevel;
     }
@@ -48,11 +48,15 @@ public class Laptop extends Computer {
 
     @Override
     public int volumeDown(int volume) {
-        if (volumeLevel <= 0) {
-            volumeLevel = 0;
-            System.out.println("Volume level 0 reached");
+        if (volume > 0) {
+            if (volumeLevel <= 0) {
+                volumeLevel = 0;
+                System.out.println("Volume level 0 reached");
+            } else {
+                volumeLevel -= volume;
+            }
         } else {
-            volumeLevel -= volume;
+            System.out.println("Wrong number, only positive numbers are allowed");
         }
         return volumeLevel;
     }
