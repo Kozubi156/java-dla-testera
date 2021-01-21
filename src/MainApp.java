23bb07f1ccfa5c907b73caa4d4ece232db4a6915
Pawel Kozubala <p.kozubala@gmail.com>
2021-01-21 05:57:20 +0100

    list, arraylist

--- src/MainApp.java
+++ src/MainApp.java
@@ -5,19 +5,12 @@ import java.util.*;
 public class MainApp {
     public static void main(String[] args) {
 
-        List<User> users = new ArrayList<>();
+        List<String> names1 = new ArrayList<>();
+        List<String> names2 = new LinkedList<>();
 
-        users.add(new User("Tomek", "Testowy", "tomek@gmail.com", 78));
-        users.add(new User("Genek", "Wisnia", "genek@gmail.com", 43));
-        users.add(new User("Paweł", "Radomski", "pawel@gmail.com", 26));
-        users.add(new User("Stanisław", "Kowalski", "stanislaw@gmail.com", 19));
+        names1.add("Bolo");
+        names2.add("Genek");
 
-        
-        Collections.sort(users, Comparator.comparing(User::getFirstName));
-        Collections.sort(users, Comparator.comparingInt(User::getAge).reversed());
-        for (User user : users) {
-            System.out.println(user.getFirstName()+" "+user.getUserAge());
-        }
 
 
 
