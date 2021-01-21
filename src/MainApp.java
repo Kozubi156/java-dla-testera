0431b9d55f530e8cb582c803babb7614da3a6fd4
Pawel Kozubala <p.kozubala@gmail.com>
2021-01-21 05:40:37 +0100

    list, arraylist

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,14 +1,23 @@
-import model.Bug;
-import model.BugReporter;
+import model.User;
+
+import java.util.*;
 
 public class MainApp {
     public static void main(String[] args) {
 
-        Bug bug1 = new Bug("Aplication doesn't work",new BugReporter("Paweł","Testowy","paweltestowy@gmail.com"),4);
-        System.out.println(bug1);
-        System.out.println(bug1.getBugReporter());
-        System.out.println(bug1.getIsActive());
-        bug1.setBugStatus(false);
+        List<User> users = new ArrayList<>();
+
+        users.add(new User("Tomek", "Testowy", "tomek@gmail.com", 78));
+        users.add(new User("Genek", "Wisnia", "genek@gmail.com", 43));
+        users.add(new User("Paweł", "Radomski", "pawel@gmail.com", 26));
+        users.add(new User("Stanisław", "Kowalski", "stanislaw@gmail.com", 19));
+
+        
+        Collections.sort(users, Comparator.comparing(User::getFirstName));
+        Collections.sort(users, Comparator.comparingInt(User::getAge).reversed());
+        for (User user : users) {
+            System.out.println(user.getFirstName()+" "+user.getUserAge());
+        }
 
 
 
